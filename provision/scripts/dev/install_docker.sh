69ac9d62f3d738b4d88db5f66aa605f3c9b88ff4
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2024-03-07 16:57:08 -0500

    feat: Add nvidia container toolkit to install_docker

--- provision/scripts/dev/install_docker.sh
+++ provision/scripts/dev/install_docker.sh
@@ -2,6 +2,34 @@
 set -e
 
 requires curl
+install_docker() {
+    #
+    # Install Docker
+    #
+    ulimit -n 65536
+    apt-get install -y \
+        docker-ce \
+        docker-ce-cli \
+        containerd.io \
+        docker-buildx-plugin \
+        docker-compose-plugin
+    systemctl enable docker.service
+    systemctl enable containerd.service
+    service docker start
+}
+install_nvidia_container_toolkit() {
+    #
+    # Install NVIDIA Container Toolkit
+    # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+    #
+    # shellcheck source=/dev/null
+    . /etc/os-release
+    curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
+    curl -s -L https://nvidia.github.io/nvidia-docker/${ID}${VERSION_ID}/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
+    apt-get update
+    apt-get install -y nvidia-container-toolkit
+    service docker restart
+}
 main() {
     # https://docs.docker.com/engine/install/debian/
     #
@@ -22,18 +50,9 @@ main() {
     chmod a+r "${KEY}"
     echo "deb [arch=$(dpkg --print-architecture) signed-by=${KEY}] https://download.docker.com/linux/${ID} ${CODENAME} stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt-get update
-    #
-    # Install Docker
-    #
-    ulimit -n 65536
-    apt-get install -y \
-        docker-ce \
-        docker-ce-cli \
-        containerd.io \
-        docker-buildx-plugin \
-        docker-compose-plugin
-    systemctl enable docker.service
-    systemctl enable containerd.service
-    service docker start
+    install_docker
+    if is_command nvidia-smi ; then
+        install_nvidia_container_toolkit
+    fi
 }
 main "$@"
