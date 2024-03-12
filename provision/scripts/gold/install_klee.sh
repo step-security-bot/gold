ead0756c1f947e0d0d84cd3dfd06772d135a34d4
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2024-03-11 21:46:03 -0400

    fix: Initial install_klee functionality (hopefully)

--- provision/scripts/gold/install_klee.sh
+++ provision/scripts/gold/install_klee.sh
@@ -13,7 +13,6 @@ install_llvm() {
         llvm-13-dev \
         llvm-13-tools
     echo 'alias clang=clang-13' >> "${HOME}/.zshrc"
-    source "${HOME}/.zshrc"
 }
 main() {
     install_llvm
@@ -40,6 +39,7 @@ main() {
     cmake -DENABLE_SOLVER_Z3=ON -DENABLE_POSIX_RUNTIME=ON -DENABLE_UNIT_TESTS=OFF -DENABLE_SYSTEM_TESTS=OFF "${KLEE_SRC_DIRECTORY}"
     make
     cleanup
+    ln -s "${KLEE_BUILD_DIRECTORY}/bin/klee" /usr/local/bin/klee
     cd "${HOME}" || exit
 }
 main "$@"
