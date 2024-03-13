733e26049e7a96818f1eade7334ec51dfdaea388
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2024-03-12 20:25:28 -0400

    feat: Add MUSL install script

---
+++ provision/scripts/dev/install_musl.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+set -e
+
+requires \
+    curl \
+    make
+main() {
+    local VERSION="${1:-"1.2.5"}"
+    curl https://musl.libc.org/releases/musl-${VERSION}.tar.gz -o /tmp/musl.tar.gz
+    tar -xvf /tmp/musl.tar.gz --directory /tmp
+    cd "/tmp/musl-${VERSION}"
+    ./configure --exec-prefix="/usr/local"
+    make && make install
+    cd "${HOME}"
+    cleanup
+}
+main "$@"
