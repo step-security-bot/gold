c98433f1f7e9246ba959e530fff05a2fc40135d8
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2024-03-21 21:02:01 -0400

    feat: Add download_notebooks script to notebook image

--- provision/scripts/notebook/download_notebooks.sh
+++ provision/scripts/notebook/download_notebooks.sh
@@ -14,15 +14,15 @@ main() {
         Clojure:clojure
         Elixir:elixir
         F#:fsharp
-        Golang:go
+        Go:go
         Haskell:haskell
         JavaScript:javascript
         Julia:julia
         Kotlin:kotlin
         Lua:lua
-        Python:python
         PowerShell:powershell
-        R:R
+        Python:python
+        R_language:R
         Ruby:ruby
         Rust:rust
         Scala:scala
@@ -32,9 +32,11 @@ main() {
     # shellcheck disable=SC2010
     NOTEBOOKS=$(ls "${NOTEBOOKS_DIR}" | grep 'ipynb$' | cut -d'.' -f1)
     EXISTING="$(echo "${DATA}" | grep "${NOTEBOOKS}$" | cut -d':' -f1)"
+    # shellcheck disable=SC2199
     if [[ -z ${NOTEBOOKS[@]} ]]; then
         SELECTED=''
     else
+        # shellcheck disable=SC2068
         SELECTED=$(echo ${EXISTING[@]} | sed -r 's/[[:blank:]]+/,/g')
     fi
     # shellcheck disable=SC2046,SC2068,SC2116
@@ -42,14 +44,12 @@ main() {
         --no-limit \
         --cursor-prefix="${EMPTY}" \
         --header="Please select notebook(s)" \
-        --selected=${SELECTED} \
+        --selected="${SELECTED}" \
         --selected.foreground="${COLOR}" \
         --selected-prefix="[x] " \
         --unselected-prefix="${EMPTY}" \
         $(echo "${CHOICES}"))
-    LANGUAGES=$(echo "${DATA}" | grep "${CHOSEN}" | cut -d':' -f2)
-    echo $LANGUAGES
-    exit
+    LANGUAGES=$(echo "${DATA}" | grep "${CHOSEN}:" | cut -d':' -f2)
     mkdir -p "${NOTEBOOKS_DIR}"
     cd "${NOTEBOOKS_DIR}" || exit
     for LANGUAGE in ${LANGUAGES}; do
