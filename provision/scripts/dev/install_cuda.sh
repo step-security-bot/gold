8778f0315fa9fd5309807b2e20a4cb8165661c3a
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2024-06-16 20:36:59 -0400

    fix: remove extra quotes from cuda script

--- provision/scripts/dev/install_cuda.sh
+++ provision/scripts/dev/install_cuda.sh
@@ -26,7 +26,7 @@ main() {
     apt-get update
     # shellcheck disable=SC2154
     curl -fSsL "https://developer.download.nvidia.com/compute/cuda/repos/${ID}${VERSION_ID}/x86_64/3bf863cc.pub" | gpg --dearmor | tee /usr/share/keyrings/nvidia-drivers.gpg > /dev/null 2>&1
-    echo "'deb [signed-by=/usr/share/keyrings/nvidia-drivers.gpg] https://developer.download.nvidia.com/compute/cuda/repos/${ID}${VERSION_ID}/x86_64/ /'" | tee /etc/apt/sources.list.d/nvidia-drivers.list
+    echo "deb [signed-by=/usr/share/keyrings/nvidia-drivers.gpg] https://developer.download.nvidia.com/compute/cuda/repos/${ID}${VERSION_ID}/x86_64/ /" | tee /etc/apt/sources.list.d/nvidia-drivers.list
     apt-get update
     apt-get install --yes \
         nvidia-driver \
