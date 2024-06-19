e6d2f2c9bedd5ac0e7f647b4571aa3d7ca1bc2a0
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2024-06-18 20:26:57 -0400

    wip: Re-add homebrew; add silent install flag

--- provision/scripts/dev/install_homebrew.sh
+++ provision/scripts/dev/install_homebrew.sh
@@ -6,7 +6,7 @@ main() {
     #
     # Install Homebrew
     #
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     # shellcheck disable=SC2016
     echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "${HOME}/.zshrc"
 }
